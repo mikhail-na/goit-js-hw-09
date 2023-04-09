104e14fb80f5440c73d57512d2228520985a31a4
mikhail-na <115450336+mikhail-na@users.noreply.github.com>
2023-04-09 18:15:50 +0000

    Deploying to gh-pages from @ mikhail-na/goit-js-hw-09@f31ec7d72aefdddbca0fa057dc46d033cccb7dc3 🚀

---
+++ 01-color-switcher.89e2f5a8.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body");t.addEventListener("click",(function(){n=setInterval(d,1e3),t.disabled=!0,t.disabled=!1})),e.addEventListener("click",(function(){clearInterval(n),e.disabled=!1,e.disabled=!0}));var n=null;function d(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}}();
+//# sourceMappingURL=01-color-switcher.89e2f5a8.js.map
